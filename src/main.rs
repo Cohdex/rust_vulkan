bdae03db4ea58041898d1db7eb57c76bf65a630c
Jonathan Cohlin <jonathan.cohlin@gmail.com>
2021-01-01 19:33:48 +0100

    Switch logger to Fern

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
 use glfw::{Action, Context, Key};
-use log4rs::{
-    append::{console::ConsoleAppender, file::FileAppender},
-    config::{Appender, Config, Root},
-    encode::pattern::PatternEncoder,
-};
 
 fn main() {
     setup_logging();
@@ -15,11 +10,13 @@ fn main() {
         .expect("Failed to create GLFW window");
 
     window.set_key_polling(true);
+    window.set_mouse_button_polling(true);
     window.make_current();
 
     log::info!("Window created");
 
     while !window.should_close() {
+        window.swap_buffers();
         glfw.poll_events();
         for (_, event) in glfw::flush_messages(&events) {
             handle_window_event(&mut window, event);
@@ -35,45 +32,40 @@ fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
             log::info!("'Escape' pressed");
             window.set_should_close(true);
         }
-        _ => {}
+        _ => log::debug!("Unhandled event: {:?}", event),
     }
 }
 
 fn setup_logging() {
-    // Use Debug level logging if debugging, otherwise Info level
+    // Use Trace level logging if debugging, otherwise Info level
     let level = if cfg!(debug_assertions) {
-        log::LevelFilter::Debug
+        log::LevelFilter::Trace
     } else {
         log::LevelFilter::Info
     };
-    let file_path = "logs/main.log";
-    // Pattern: https://docs.rs/log4rs/*/log4rs/encode/pattern/index.html
-    let pattern = "{d(%Y-%m-%d %H:%M:%S.%3f)} [{h({l:>5})}] {M}: {m}{n}";
 
-    // Build a stderr logger
-    let stdout = ConsoleAppender::builder()
-        .encoder(Box::new(PatternEncoder::new(pattern)))
-        .build();
-
-    // Logging to log file
-    let logfile = FileAppender::builder()
-        .encoder(Box::new(PatternEncoder::new(pattern)))
-        .build(file_path)
-        .unwrap();
-
-    let config = Config::builder()
-        .appender(Appender::builder().build("stdout", Box::new(stdout)))
-        .appender(Appender::builder().build("logfile", Box::new(logfile)))
-        .build(
-            Root::builder()
-                .appender("stdout")
-                .appender("logfile")
-                .build(level),
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{} [{:>5}] {}: {}",
+                chrono::Local::now().format("%Y-%m-%d %H:%M:%S.%3f"),
+                record.level(),
+                record.target(),
+                message
+            ))
+        })
+        .level(level)
+        .chain(std::io::stdout())
+        .chain(
+            std::fs::OpenOptions::new()
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open("logs/main.log")
+                .expect("Failed to create main log file"),
         )
+        .apply()
         .unwrap();
-
-    // Use handle to change log levels at runtime
-    let _handle = log4rs::init_config(config).expect("Failed to initialize logging");
 
     log::debug!("Logging initialized");
 }
