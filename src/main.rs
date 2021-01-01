88c4d9ade1299af4f6f5b7c74549c00679efcf34
Jonathan Cohlin <jonathan.cohlin@gmail.com>
2021-01-01 02:13:33 +0100

    Create window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use glfw::{Action, Context, Key};
 use log4rs::{
     append::{console::ConsoleAppender, file::FileAppender},
     config::{Appender, Config, Root},
@@ -7,11 +8,42 @@ use log4rs::{
 
 fn main() {
     setup_logging();
+
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).expect("Failed to initialize GLFW");
+
+    let (mut window, events) = glfw
+        .create_window(1280, 720, "Rust Vulkan Example", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window");
+
+    window.set_key_polling(true);
+    window.make_current();
+
+    log::info!("Window created");
+
+    while !window.should_close() {
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            handle_window_event(&mut window, event);
+        }
+    }
+
+    log::info!("Shutting down...");
+}
+
+fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
+    match event {
+        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+            log::info!("'Escape' pressed");
+            window.set_should_close(true);
+        }
+        _ => {}
+    }
 }
 
 fn setup_logging() {
-    let level = if cfg!(debug_assertions) {
-        log::LevelFilter::Trace
+    // Use Debug level logging to stdout if debugging, otherwise Info level
+    let stdout_level = if cfg!(debug_assertions) {
+        log::LevelFilter::Debug
     } else {
         log::LevelFilter::Info
     };
@@ -20,7 +52,7 @@ fn setup_logging() {
     let pattern = "{d(%Y-%m-%d %H:%M:%S.%3f)} [{h({l:>5})}] {M}: {m}{n}";
 
     // Build a stderr logger.
-    let stderr = ConsoleAppender::builder()
+    let stdout = ConsoleAppender::builder()
         .encoder(Box::new(PatternEncoder::new(pattern)))
         .build();
 
@@ -30,20 +62,19 @@ fn setup_logging() {
         .build(file_path)
         .unwrap();
 
-    // Log Trace level output to file where trace is the default level
-    // and the programmatically specified level to stderr.
+    // Log Info level output to file and the programmatically specified level to stdout.
     let config = Config::builder()
         .appender(Appender::builder().build("logfile", Box::new(logfile)))
         .appender(
             Appender::builder()
-                .filter(Box::new(ThresholdFilter::new(level)))
-                .build("stderr", Box::new(stderr)),
+                .filter(Box::new(ThresholdFilter::new(stdout_level)))
+                .build("stdout", Box::new(stdout)),
         )
         .build(
             Root::builder()
+                .appender("stdout")
                 .appender("logfile")
-                .appender("stderr")
-                .build(log::LevelFilter::Trace),
+                .build(log::LevelFilter::Info),
         )
         .unwrap();
 
@@ -51,7 +82,7 @@ fn setup_logging() {
     // This means you can change the default log level to trace
     // if you are trying to debug an issue and need more logs on then turn it off
     // once you are done.
-    let _handle = log4rs::init_config(config).unwrap();
+    let _handle = log4rs::init_config(config).expect("Failed to initialize logging");
 
     log::debug!("Logging initialized");
 }
